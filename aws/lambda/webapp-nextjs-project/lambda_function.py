bc902dcf4f99ade9253b14b45db760923ab916e1
Yuki Kitayama <yk2797@columbia.edu>
2022-07-11 00:48:55 +0000

    deployed AWS Lambda of adding a new task to Cassandra

--- aws/lambda/webapp-nextjs-project/lambda_function.py
+++ aws/lambda/webapp-nextjs-project/lambda_function.py
@@ -1,14 +1,18 @@
 from cassandra.cluster import Cluster
-from ssl import SSLContext, PROTOCOL_TLSv1_2, CERT_REQUIRED
+from cassandra import ConsistencyLevel
+from cassandra.query import SimpleStatement
 from cassandra.auth import PlainTextAuthProvider
 import boto3
+from ssl import SSLContext, PROTOCOL_TLSv1_2, CERT_REQUIRED
 import json
 import uuid
+import pprint
 
 
 CERTIFICATE = './sf-class2-root.crt'
 SECRET_ID = 'cassandra'
 REGION_NAME = 'us-west-1'
+KEYSPACE = 'webapp'
 
 
 def get_secret(secret_id: str, region_name: str) -> dict:
@@ -19,57 +23,77 @@ def get_secret(secret_id: str, region_name: str) -> dict:
     return json.loads(secret_string)
 
 
+# Get secrets
 secret = get_secret(SECRET_ID, REGION_NAME)
 username = secret['username']
 password = secret['password']
 
+# Create a Cassandra cluster instance
+ssl_context = SSLContext(PROTOCOL_TLSv1_2)
+ssl_context.load_verify_locations(CERTIFICATE)
+ssl_context.verify_mode = CERT_REQUIRED
+auth_provider = PlainTextAuthProvider(
+    username=username, 
+    password=password
+)
+cluster = Cluster(
+    ['cassandra.us-west-1.amazonaws.com'], 
+    ssl_context=ssl_context, 
+    auth_provider=auth_provider, 
+    port=9142
+)
+# Establish connections
+session = cluster.connect(KEYSPACE)
 
-def main():
-    ssl_context = SSLContext(PROTOCOL_TLSv1_2)
-    ssl_context.load_verify_locations(CERTIFICATE)
-    ssl_context.verify_mode = CERT_REQUIRED
-    auth_provider = PlainTextAuthProvider(
-        username=username, 
-        password=password
-    )
-    cluster = Cluster(
-        ['cassandra.us-west-1.amazonaws.com'], 
-        ssl_context=ssl_context, 
-        auth_provider=auth_provider, 
-        port=9142
-    )
-    session = cluster.connect()
+
+def add_row(event):
     
-    # r = session.execute('select * from system_schema.keyspaces')
-    # for row in r.current_rows:
-    #     print(row)
-
+    # Get user input
+    body = json.loads(event['body'])
+    labels = ','.join(f"'{word}'" for word in body['labels'].split(','))
+    # print(f'labels: {labels}')
     id_ = uuid.uuid4()
-    print(id_)
-
     query = f"""
-        INSERT INTO webapp.project (
-            id,
-            project,
-            task,
-            priority,
-            start_date,
-            due_date,
-            labels
-        )
-        VALUES (
-            {id_},
-            'project a',
-            'task a',
-            'medium',
-            '2022-07-09',
-            '2022-07-16',
-            ['label 1', 'label 2']
-        );
+        INSERT INTO webapp.project (id, project, task, priority, start_date, 
+            due_date, labels)
+        VALUES ({id_}, '{body["project"]}', '{body["task"]}', 
+            '{body["priority"]}', '{body["startDate"]}', '{body["dueDate"]}',
+            [{labels}]);
     """
-    r = session.execute(query)
-    # print(r)
+    print(query)
+    # Wrap queries to specify the consistency level for write
+    wrapped_query = SimpleStatement(
+        query,
+        consistency_level=ConsistencyLevel.LOCAL_QUORUM
+    )
+    session.execute(wrapped_query)
+    print(f'Uploaded to Cassandra the data with ID: {id_}')
+
+def lambda_handler(event, context):
+    
+    headers = { 'Access-Control-Allow-Origin': '*' }
+    
+    if event['httpMethod'] == 'POST':
+        
+        add_row(event)
+    
+        return {
+            'statusCode': 200,
+            'headers': headers,
+            'body': json.dumps({ 'message': 'Added the new task successfully' })
+        }
 
 
 if __name__ == '__main__':
-    main()
+    event = {
+        'httpMethod': 'POST',
+        'body': json.dumps({
+            'project': 'webapp',
+            'task': 'do task 1',
+            'priority': 'medium',
+            'startDate': '2022-07-10',
+            'dueDate': '2022-07-17',
+            'labels': 'python,javascript'
+        })
+    }
+    pprint.pprint(lambda_handler(event, ''))
